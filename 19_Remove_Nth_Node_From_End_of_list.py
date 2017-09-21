6e749a1992b59d6384c05f02d023168679ab0f8b
zhanzecheng <50673223@qq.com>
2017-09-21 22:57:39 +0800

    使用龟兔赛跑的方式对链表进行处理

---
+++ 19_Remove_Nth_Node_From_End_of_list.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+# @Time    : 2017/9/21 下午10:24
+# @Author  : zhanzecheng
+# @File    : 19_Remove_Nth_Node_From_End_of_list.py
+# @Software: PyCharm
+"""
+
+'''
+这个采用了一个比较简单的方法，就是二者追逐法
+'''
+'''
+ Given linked list: 1->2->3->4->5, and n = 2.
+
+   After removing the second node from the end, the linked list becomes 1->2->3->5.
+'''
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        fast = slow = head
+        for _ in range(n):
+            fast = fast.next
+        if not fast:
+            return head.next
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return head
+
+
+def stringToIntegerList(input):
+    input = input.strip()
+    input = input[1:-1]
+    return [int(number) for number in input.split(",")]
+
+def stringToListNode(input):
+
+    numbers = stringToIntegerList(input=input)
+
+    # Now convert that list into linked list
+    dummyRoot = ListNode(0)
+    ptr = dummyRoot
+    for number in numbers:
+        ptr.next = ListNode(number)
+        ptr = ptr.next
+
+    ptr = dummyRoot.next
+    return ptr
+
+def listNodeToString(node):
+    result = ""
+    while node:
+        result += str(node.val) + ", "
+        node = node.next
+    return result[:-2]
+
+import sys
+
+def readlines():
+    for line in sys.stdin:
+        yield line.strip('\n')
+
+def main():
+    lines = readlines()
+    while True:
+        try:
+            line = lines.next()
+            head = stringToListNode(line)
+            line = lines.next()
+            n = int(line)
+
+            ret = Solution().removeNthFromEnd(head, n)
+
+            out = listNodeToString(ret)
+            print out
+        except StopIteration:
+            break
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
