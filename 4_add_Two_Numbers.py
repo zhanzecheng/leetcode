ee869e4b2de97c5f57e8c9bbc8a952f517f24764
zhanzecheng <50673223@qq.com>
2017-09-24 16:34:51 +0800

    很简单的链表加减题

---
+++ 4_add_Two_Numbers.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+# @Time    : 2017/9/24 下午4:30
+# @Author  : zhanzecheng
+# @File    : 4_add_Two_Numbers.py
+# @Software: PyCharm
+"""
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        carry = 0
+        root = n = ListNode(0)
+        while l1 or l2 or carry:
+            v1 = v2 = 0
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
+            carry, val = divmod(v1 + v2 + carry, 10)
+            n.next = ListNode(val)
+            n = n.next
+        return root.next
+
